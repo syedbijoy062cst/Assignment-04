35af95b6cc5b41f46b46d213c32ef909c32fb667
syedbijoy062cst <syedbijoy062@gmail.com>
2026-03-12 11:45:57 +0600

    added a index.js btn function click

---
+++ script/index.js
@@ -0,0 +1,46 @@
+let currentTab = "all";
+const tabActive = ["bg-primary" ];
+const tabInactive = ["bg-transparent"];
+
+const allContainer = document.getElementById("all-containar");
+const interviewContainer = document.getElementById("Interview-container");
+
+const rejectedContainer = document.getElementById("Rejected-container");
+
+
+
+
+
+function switchTab(tab){
+    // console.log(tab);
+    const tabs = ["all", "interview", "rejected"];
+    for(const t of tabs){
+        const tabName = document.getElementById("tab-" + t);
+        if(t === tab) {
+            tabName.classList.remove(...tabInactive);
+            tabName.classList.add(...tabActive);
+
+        }
+        else{
+            tabName.classList.remove(...tabActive);
+            tabName.classList.add(...tabInactive);
+        }
+    }
+    
+    const pages = [allContainer, interviewContainer, rejectedContainer];
+    for(const section of pages){
+        section.classList.add("hidden");
+    }
+
+    if(tab === "all"){
+        allContainer.classList.remove("hidden");
+    }
+    else if (tab === "interview") {
+        interviewContainer.classList.remove("hidden");
+    }
+    else{
+        rejectedContainer.classList.remove("hidden");
+    }
+
+}
+switchTab(currentTab);
